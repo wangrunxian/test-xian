8ac9c98a73c2b504d819e86bbb9305b776875c75
wangrunxian <wangrunxian@trendit.cn>
2023-03-07 17:49:12 +0800

    my first commit

---
+++ hello.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+
+int main(int argc , char const *argv[])
+{
+	int num;
+	scanf("%d" , &num);
+
+	if(100 == num)
+	{
+		printf("This num is %d !\n" , num);
+	}
+	else
+	{
+		printf("Hello World and Linux!");
+	}
+
+	return 0;
+
+}
+
+
